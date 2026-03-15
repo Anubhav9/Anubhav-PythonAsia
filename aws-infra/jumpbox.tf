9ead6bdb3c141909f9545be415ee864fd60844c6
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-15 10:50:49 +0530

    region chnged to tokyo

--- aws-infra/jumpbox.tf
+++ aws-infra/jumpbox.tf
@@ -1,8 +1,18 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
 resource "aws_instance" "python_asia_jumbox" {
-  ami = var.ami_ec2
-  instance_type = var.instance_size_ec2
-  subnet_id = aws_subnet.python_asia_public_subnet.id
-  key_name = "PythonAsiaTalkKeyValuePair"
+  ami                    = data.aws_ami.amazon_linux_2023.id
+  instance_type          = var.instance_size_ec2
+  subnet_id              = aws_subnet.python_asia_public_subnet.id
+  key_name               = "PythonAsiaTalkKeyValuePair"
   vpc_security_group_ids = [aws_security_group.python_asia_ec2_security_group.id]
 
   associate_public_ip_address = true
@@ -11,5 +21,5 @@ resource "aws_instance" "python_asia_jumbox" {
     Name = "python-asia-jumpbox"
   }
 
-  
+
 }
