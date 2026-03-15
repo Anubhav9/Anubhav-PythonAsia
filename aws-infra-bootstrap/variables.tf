031a49f9270a14784f37f9a3b1ccfde287003602
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-15 10:52:02 +0530

    Create variables.tf

---
+++ aws-infra-bootstrap/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region for Terraform backend resources"
+  default     = "ap-northeast-1"
+}
+
+variable "state_bucket_name" {
+  type        = string
+  description = "S3 bucket name for Terraform remote state"
+}
+
+variable "lock_table_name" {
+  type        = string
+  description = "DynamoDB table name for Terraform state lock"
+}
