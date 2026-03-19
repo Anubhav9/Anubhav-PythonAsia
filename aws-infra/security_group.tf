663d3479a136409a44c15aa8ed20f35856ec8e54
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-20 06:50:38 +0800

    added connectivity from EKS

--- aws-infra/security_group.tf
+++ aws-infra/security_group.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "python_asia_ec2_security_group" {
-  vpc_id = aws_vpc.python_asia_vpc.id
-  name = "EC2 Security Group"
+  vpc_id      = aws_vpc.python_asia_vpc.id
+  name        = "EC2 Security Group"
   description = "Allow SSH into EC2 instance"
-  
+
 }
 
 resource "aws_vpc_security_group_ingress_rule" "python_asia_ec2_ingress_rule" {
   security_group_id = aws_security_group.python_asia_ec2_security_group.id
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port = 22
-  to_port = 22
-  ip_protocol = "tcp"
-  
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+
 }
 
 resource "aws_vpc_security_group_egress_rule" "python_asia_ec2_egress_rule" {
@@ -21,17 +21,24 @@ resource "aws_vpc_security_group_egress_rule" "python_asia_ec2_egress_rule" {
 }
 
 resource "aws_security_group" "python_asia_rds_security_group" {
-  vpc_id = aws_vpc.python_asia_vpc.id
-  name = "RDS Security Group"
+  vpc_id      = aws_vpc.python_asia_vpc.id
+  name        = "RDS Security Group"
   description = "Allow Ingress into RDS"
-  
+
 }
 
 resource "aws_vpc_security_group_ingress_rule" "python_asia_rds_ingress_rule" {
-  security_group_id = aws_security_group.python_asia_rds_security_group.id
+  security_group_id            = aws_security_group.python_asia_rds_security_group.id
   referenced_security_group_id = aws_security_group.python_asia_ec2_security_group.id
-  from_port = 5432
-  to_port = 5432
-  ip_protocol = "tcp"
-  
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "python_asia_rds_eks_ingress_rule" {
+  security_group_id = aws_security_group.python_asia_rds_security_group.id
+  cidr_ipv4         = aws_vpc.python_asia_vpc.cidr_block
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
 }
